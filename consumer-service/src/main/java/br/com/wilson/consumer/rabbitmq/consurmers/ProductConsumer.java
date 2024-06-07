a012aac9a930b88344a6a8683467cad8e13a7252
wilson <wilson-gs@hotmail.com>
2024-06-07 15:46:15 -0300

    implementado producer e consumer para enviar objeto

--- consumer-service/src/main/java/br/com/wilson/consumer/rabbitmq/consurmers/ProductConsumer.java
+++ consumer-service/src/main/java/br/com/wilson/consumer/rabbitmq/consurmers/ProductConsumer.java
@@ -1,5 +1,6 @@
 package br.com.wilson.consumer.rabbitmq.consurmers;
 
+import dtos.ProductDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -8,11 +9,11 @@ import static constants.RabbitMQConstants.RK_PRODUCT_LOG;
 
 @Log4j2
 @Component
-public class StringConsumer {
+public class ProductConsumer {
 
     @RabbitListener(queues = {RK_PRODUCT_LOG})
-    public void consumer(String message){
-        log.info("Consumer received a message " + message);
+    public void consumerLog(ProductDTO message){
+        log.info("Consumer received a product " + message.toString());
     }
 
 }
